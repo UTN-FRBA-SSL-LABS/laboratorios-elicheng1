0f7bbd4f524d357f4b264289e5bd5bec60e31c5e
Cheng Eli <echeng@frba.utn.edu.ar>
2026-08-23 23:26:56 -0300

    se sube laboratorio_testing_c

--- labs/laboratorio-testing-c/tests/test_integracion.c
+++ labs/laboratorio-testing-c/tests/test_integracion.c
@@ -10,20 +10,46 @@
 /* ═══════════════════════════════════════════════════════════════════════════
  *  PARTE D — Escribir el test guiado (ver README.md, Parte 8)
  * ═══════════════════════════════════════════════════════════════════════════ */
-
+void test_compra_con_descuento(){
+    printf("\n[compra con descuento]\n");
+    Carrito c;
+    carrito_init(&c);
+    Producto p={"Pan", 200, 3};
+    Producto L={"Leche", 350, 2};
+    carrito_agregar(&c, p);
+    carrito_agregar(&c, L);
+    ASSERT_IGUAL(1300, carrito_total(&c));
+    ASSERT_IGUAL(1170, carrito_descuento(carrito_total(&c), 10));
+}
 /* TODO: escribir test_compra_con_descuento() siguiendo la guia del .md */
 
 /* ═══════════════════════════════════════════════════════════════════════════
  *  PARTE E — Disenar un test propio (ver README.md, Parte 9)
  * ═══════════════════════════════════════════════════════════════════════════ */
-
+void test_agregar_hasta_llenar(){
+    printf("\n[agregar hasta llenar]\n");
+    Carrito c;
+    carrito_init(&c);
+    Producto p = {"Leche", 350, 1};
+    Producto x = {"Manteca", 350, 1};
+    Producto y = {"Yogurt", 350, 1};
+    Producto z = {"Crema", 350, 1};
+    Producto t = {"Leche", 350, 1};
+    carrito_agregar(&c, p);
+    carrito_agregar(&c, x);
+    carrito_agregar(&c, y);
+    carrito_agregar(&c, z);
+    ASSERT_IGUAL(MAX_ITEMS, carrito_contar(&c));       
+    ASSERT_IGUAL(0, carrito_agregar(&c, t));            
+    ASSERT_IGUAL(MAX_ITEMS, carrito_contar(&c)); 
+}
 /* TODO: escribir test_agregar_hasta_llenar() */
 
 int main(void) {
     printf("=== Tests de integracion ===");
     /* Descomentar a medida que agregues las funciones: */
-    /* test_compra_con_descuento();  */
-    /* test_agregar_hasta_llenar();  */
+    test_compra_con_descuento();
+    test_agregar_hasta_llenar();
     RESUMEN();
     return EXIT_CODE();
 }
